5a91c68bfbc833c642553af905caa8950c8a1abf
martywebdev <martywebdevelopment@gmail.com>
2025-09-06 20:22:09 +0800

    capture webcam video

---
+++ emailing.py
@@ -0,0 +1,19 @@
+import cv2
+import time
+import numpy as np
+
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+while True:
+
+    check, frame = video.read()
+    cv2.imshow('My Video', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
